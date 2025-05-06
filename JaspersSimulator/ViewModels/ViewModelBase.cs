8022e698d078f326ee953883491567528e2951b7
markoaleskovic <malesko@lagebra.hr>
2025-05-06 21:41:48 +0200

    Added Jaspers Simulator project and configured references and assembly

---
+++ JaspersSimulator/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace JaspersSimulator.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
--- Nodify.Shared/Properties/AssemblyInfo.cs
+++ Nodify.Shared/Properties/AssemblyInfo.cs
@@ -8,3 +8,4 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("JaspersSimulator, PublicKey=00240000048000009400000006020000002400005253413100040000010001007def71791fed4425f4f19ad05172ab68a8a098b8e2964ccc31ede102ed96d8ed7e493b7c6092d49ce0a2904969624cdff7a57a3b66ed80616c318c98d00efa5d0f8045371d9e468e9d1a507060e06747124bebd699c25b9afbeee78d74fcff703ba54123c73f2e57760fc9eda4c97572b8d7b5e33940cc568e20b131ad26acec")]
